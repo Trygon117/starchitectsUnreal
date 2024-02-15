f89d767984a52c7f8a65eb1e8e39eb578d3fe904
Abraham Furlan <abrahamfurlan@gmail.com>
2024-02-15 18:46:49 -0500

    Websocket Connection status shows in top left corner

--- Source/Starchitects/StarchitectsGameInstance.cpp
+++ Source/Starchitects/StarchitectsGameInstance.cpp
@@ -13,6 +13,26 @@ void UStarchitectsGameInstance::Init() {
 
     WebSocket = FWebSocketsModule::Get().CreateWebSocket("ws://127.0.0.1:443");
 
+    WebSocket->OnConnected().AddLambda([]() {
+        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, "Successfully Connected");
+    });
+
+    WebSocket->OnConnectionError().AddLambda([](const FString& Error) {
+        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, Error);
+    });
+
+    WebSocket->OnClosed().AddLambda([](int32 StatusCode, const FString& Reason, bool bWasClean) {
+        GEngine->AddOnScreenDebugMessage(-1, 15.0f, bWasClean ? FColor::Green : FColor::Red, "Connection Closed: " + Reason);
+    });
+
+    WebSocket->OnMessage().AddLambda([](const FString& Message) {
+        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, "Recieved Message: " + Message);
+    });
+
+    WebSocket->OnMessageSent().AddLambda([](const FString& Message) {
+        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "Sent Message: " + Message);
+    });
+
     WebSocket->Connect();
 }
 
