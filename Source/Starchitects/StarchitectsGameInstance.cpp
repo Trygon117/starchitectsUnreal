391fda3a72d6dcaa3e8431bcfb1c4f0b9c0911fa
Abraham Furlan <abrahamfurlan@gmail.com>
2024-02-15 19:58:41 -0500

    Added an initial websocket message

--- Source/Starchitects/StarchitectsGameInstance.cpp
+++ Source/Starchitects/StarchitectsGameInstance.cpp
@@ -1,4 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+// Youtube Tutorial for Websockets: https://www.youtube.com/watch?v=l9TTmtDBTWY
+// WebSockets Documentation: https://docs.unrealengine.com/4.27/en-US/API/Runtime/WebSockets/
 
 
 #include "StarchitectsGameInstance.h"
@@ -13,8 +15,9 @@ void UStarchitectsGameInstance::Init() {
 
     WebSocket = FWebSocketsModule::Get().CreateWebSocket("ws://127.0.0.1:443");
 
-    WebSocket->OnConnected().AddLambda([]() {
+    WebSocket->OnConnected().AddLambda([&]() {
         GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, "Successfully Connected");
+        WebSocket->Send("0");
     });
 
     WebSocket->OnConnectionError().AddLambda([](const FString& Error) {
@@ -32,6 +35,9 @@ void UStarchitectsGameInstance::Init() {
     WebSocket->OnMessageSent().AddLambda([](const FString& Message) {
         GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "Sent Message: " + Message);
     });
+
+    // example send message
+    // WebSocket->Send("Test Message");
 
     WebSocket->Connect();
 }
